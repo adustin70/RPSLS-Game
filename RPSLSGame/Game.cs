a15c1b973245f91507e969b7c1d5bd13cd8a121a
Dustin <adustin70@gmail.com>
2021-01-14 15:43:07 -0500

    New repo

---
+++ RPSLSGame/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSGame
+{
+    class Game
+    {
+        //member variables
+        Player playerOne;
+        Player playerTwo;
+
+        //constructor
+        public Game()
+        {
+            playerOne = new Player();
+            playerTwo = new Player();
+        }
+        //member methods
+
+        public void DisplayRules()
+        {
+            Console.WriteLine("Select solo or Co-op then select your gestures player with most rounds won wins the game.");
+        }
+
+        public void SelectNumberOfPlayers()
+        {
+
+        }
+
+
+        //1. Display the rules
+        //2. Single Player or Multiplayer?
+        //3. Who goes first? Choose a gesture.
+        //4. Next person chooses a gesture.
+        //5. Make a comparison between the two gestures.
+        //6. Whoever has the winning gesture wins the round.
+        //7. Repeat until best of three.
+        //8. Display who the winner is!
+    }
+}
---
+++ RPSLSGame/Paper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSGame
+{
+    class Paper : Gestures
+    {
+        //member variables
+
+        //constructor
+
+        //member methods
+    }
+}
